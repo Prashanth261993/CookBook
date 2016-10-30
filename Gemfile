fb654b343dca17b4bb8c138686303463b26bed19
Prashanth261993 <prashanthr261993@gmail.com>
2016-10-30 02:02:17 -0400

    added clarifai

--- Gemfile
+++ Gemfile
@@ -45,6 +45,8 @@ gem 'chosen-rails'
 
 gem 'bootstrap-chosen-rails'
 
+gem 'clarifai_ruby'
+
 group :development, :test do
   # Call 'byebug' anywhere in the code to stop execution and get a debugger console
   gem 'byebug', platform: :mri
---
+++ config/initializers/clarifai_ruby.rb
@@ -0,0 +1,6 @@
+ClarifaiRuby.configure do |config|
+  config.base_url       = "https://api.clarifai.com"
+  config.version_path   = "/v1"
+  config.client_id      = "6EmIZyFgL1rBo3SFUWX5SeLSa8ao8h6xGrCuxj3m"
+  config.client_secret  = "HVjrfi_XT_6s1St89PYVWIEcVtnLI0hBlJwHAwRQ"
+end
